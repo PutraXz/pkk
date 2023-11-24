b94e221c8921edbf9fc1887dbccaa45567d321ab
PutraXz <91056167+PutraXz@users.noreply.github.com>
2023-11-24 09:07:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware('auth')->group(function () {
     Route::group(['middleware' => 'check-level:user'], function (){
         Route::get('/product', UserTheme::class);
         Route::post('/product', [ToChartController::class, 'chart']);
-        Route::get('/check-out', CheckShopping::class)->name('check-out');
+        Route::get('/check-out', User\Shopping\CheckShopping::class)->name('check-out');
         Route::post('check-out', [WebController::class, 'pay']);
         Route::get('confirm', [ShoppingController::class, 'confirm']);
         Route::delete('check-out/{id}', [ShoppingController::class, 'destroy']);
